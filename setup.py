e63db2b24e6480a58be0734303c3c056fdc829c1
Haak Saxberg <haak.erling@gmail.com>
2012-08-30 14:31:17 -0700

    find_packages instead of manual, and fixing name of bin folder

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from setuptools import find_packages
 import os
 
 setup(
-    name="Flowhub",
+    name="flowhub",
     version='0.2',
     description="Git-flow adapted for GitHub",
     author="Haak Saxberg",
     url="http://github.com/haaksmash/flowhub",
-    packages=['flowhub'],
+    packages=find_packages(),
     scripts=[
-        os.path.join('scripts','flowhub')
+        os.path.join('bin','flowhub')
     ]
 )
