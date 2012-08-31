16aad5ce2034106eac4438498e0a9c6eeb5a4c62
Haak Saxberg <haak.erling@gmail.com>
2012-08-30 17:12:45 -0700

    decorator file added, with_summary the only thing in it now

---
+++ flowhub/decorators.py
@@ -0,0 +1,18 @@
+import functools
+
+
+def with_summary(f):
+    """Prints a nice summary, assuming a function returns a list of
+    summary strings."""
+    @functools.wraps(f)
+    def wrapper(*args, **kwargs):
+        summary = []
+        f(*args, **kwargs)
+        if summary:
+            summary = ['Summary of actions:'] + summary
+            print "\n - ".join(summary)
+
+        else:
+            print "No summary provided."
+
+    return wrapper
