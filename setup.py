e7de3069e28e7e92162db54f7786f971c0043922
Haak Saxberg <haak.erling@gmail.com>
2013-06-10 01:06:11 -0700

    bumping version number to 0.5.0

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ import os
 
 setup(
     name="flowhub",
-    version='0.4.6',
+    version='0.5.0',
     description="Git-flow adapted for GitHub",
     long_description=open("README.txt").read(),
     author="Haak Saxberg",
