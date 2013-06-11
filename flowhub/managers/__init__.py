016fced8e8695ffad350c9fff68d1f14dcafb5be
Haak Saxberg <haak.erling@gmail.com>
2013-06-11 01:24:57 -0700

    adds managers for hotfixes and releases

--- flowhub/managers/__init__.py
+++ flowhub/managers/__init__.py
@@ -18,6 +18,9 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 """
+from collections import namedtuple
+
+TagInfo = namedtuple("TagInfo", ["label", "message"])
 
 
 class Manager(object):
