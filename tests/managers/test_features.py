f9cdfa7b7a1b40fbd2210bc12aea0e47d0617933
Haak Saxberg <haak.erling@gmail.com>
2013-11-14 01:03:17 -0800

    typo fix

--- tests/managers/test_features.py
+++ tests/managers/test_features.py
@@ -19,7 +19,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 """
 
-from flowhub.managers.features import FeatureManager
+from flowhub.managers.feature import FeatureManager
 
 from managers import ManagerTestCase
 
